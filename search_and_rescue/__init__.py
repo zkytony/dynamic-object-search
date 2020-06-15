3686a92f78fe1a17052753cc804688559e8313c2
Kaiyu Zheng <kaiyutony@gmail.com>
2020-06-15 19:50:11 -0400

    Include SAR as new package

---
+++ search_and_rescue/__init__.py
@@ -0,0 +1,15 @@
+from search_and_rescue.searcher.agent import *
+from search_and_rescue.suspect.agent import *
+from search_and_rescue.victim.agent import *
+from search_and_rescue.env.state import *
+from search_and_rescue.env.action import *
+from search_and_rescue.env.visual import *
+from search_and_rescue.env.action import *
+from search_and_rescue.models.grid_map import *
+from search_and_rescue.models.sensor import *
+from search_and_rescue.models.transition_model import *
+from search_and_rescue.models.observation_model import *
+from search_and_rescue.models.policy_model import *
+from search_and_rescue.models.reward_model import *
+from search_and_rescue.models.motion_policy import *
+
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 from distutils.extension import Extension
 
 setup(name='dynamic_mos',
-      packages=['dynamic_mos', 'adversarial_mos'],
+      packages=['dynamic_mos', 'adversarial_mos', 'search_and_rescue'],
       version='0.0',
       description='Dynamic multi-object search',
       python_requires='>3.6',
